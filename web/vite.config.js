b5a0173f5fdfa6e47959c21433b6d1bbd1020c6a
何红妤#A05018 <hongyu.he@casstime.com>
2025-11-15 15:44:55 +0800

    代理

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // 以/api开头的请求走代理
+      '/api': {
+        target: 'http://localhost:5000', // 后端地址
+        changeOrigin: true
+      }
+    }
+  }
 })
