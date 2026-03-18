ddbc2f7425158f831c8ea88738c2968b97dbf731
何红妤#A05018 <hongyu.he@casstime.com>
2026-03-18 21:04:54 +0800

    添加multer库处理文件信息

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = path.join(__dirname, 'uploads');
+    if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);  // 使用原始文件名
+  }
+})
+
+const upload = multer({ storage });
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -13,6 +27,15 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
+
+// formData方式上传文件
+app.post('/formData/upload', upload.single('file'), (req, res) => {
+  res.json({
+    message: '上传成功',
+    size: req.file.size,
+    fileName: req.file.filename
+  })
+})
 
 // 普通文件上传接口
 app.post('/upload', async (req, res) => {
